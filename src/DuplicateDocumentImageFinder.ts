d07fb11394de992e5e4b0a074ca57b99f76ca913
xulihang <xulihanghai@163.com>
2024-04-22 17:35:39 +0800

    include opencv

--- src/DuplicateDocumentImageFinder.ts
+++ src/DuplicateDocumentImageFinder.ts
@@ -1,4 +1,12 @@
 export class DuplicateDocumentImageFinder {
+  private cv:any;
+  constructor() {
+    if (!("cv" in window)) {
+      throw new Error("OpenCV not found");
+    }else{
+      this.cv = window["cv"];
+    }
+  }
   detect(_source1:HTMLImageElement,_source2:HTMLImageElement):boolean{
     return true;
   }
